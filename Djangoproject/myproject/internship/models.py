50c1ff135a27850db5f1a7edc74ff02bc2cc45d5
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-04-13 13:38:41 +0300

    added user field

--- Djangoproject/myproject/internship/models.py
+++ Djangoproject/myproject/internship/models.py
@@ -34,6 +34,10 @@ class User(AbstractUser):
         
         def __str__(self):
             return self.user.username
+        
+
+class work_place_supervisor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
         
         
