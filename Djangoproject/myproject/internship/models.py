ed4867be74e22b146f1e453c4edcc3a7dd193fd3
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-04-14 16:03:52 +0300

    added a way to represent the internship_placement model in string form

--- Djangoproject/myproject/internship/models.py
+++ Djangoproject/myproject/internship/models.py
@@ -99,3 +99,7 @@ class internshipPlacement(models.Model):
      position = models.CharField(max_length= 100)
      start_date= models.DateField()
      end_date = models.DateField()
+
+
+     def __str__(self):
+        return f"{self.student.user.username} - {self.company_name}"
