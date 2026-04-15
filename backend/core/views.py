99e510d625a66c231ca5a3abaaeafaa10986a48f
suubigeorge <georgesuubi98@gmail.com>
2026-04-16 00:20:48 +0300

    backend update

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome to ILES System")
+    return render(request, 'base.html')
+    #return HttpResponse("Welcome to ILES System")
 
 def student_list(request):
     students = Student.objects.all()
