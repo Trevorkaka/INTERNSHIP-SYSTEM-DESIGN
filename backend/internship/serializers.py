0d74eb50021a6e178b9cf8376de0bd0ac568e696
eriapaulmwesigwa <eriapaul40@gmail.com>
2026-04-17 10:08:35 +0300

    added evaluation and evaluation criteria serializers

---
+++ backend/internship/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from internship.models import Assessment, Evaluation, EvaluationCriteria, InternshipPlacement, User, Student, WorkPlaceSupervisor, AcademicSupervisor, WeeklyLog
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True) #Shows all the students details. this is a nested serializer that allows us to access the related user details when we serialize a student instance.
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class WorkPlaceSupervisorSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = WorkPlaceSupervisor
+        fields = '__all__'
+
+class AcademicSupervisorSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = AcademicSupervisor
+        fields = '__all__'
+
+class WeeklyLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeeklyLog
+        fields = '__all__'
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assessment
+        fields = '__all__'
+
+class EvaluationCriteriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EvaluationCriteria
+        fields = '__all__'
+
+class EvaluationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluation
+        fields = '__all__'
+
+
+
+
+    
+
+
+
+
