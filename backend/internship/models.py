36cfefb5167e39e7bb512d6058cacd23b5cf3f4f
eriapaulmwesigwa <eriapaul40@gmail.com>
2026-04-17 09:46:45 +0300

    Added user_serialiser

--- backend/internship/models.py
+++ backend/internship/models.py
@@ -12,7 +12,20 @@ class User(AbstractUser):
         ('admin', 'Administrator'),
         )
     role = models.CharField(max_length=30, choices = ROLE_CHOICES )
-
+    @property
+    def is_student(self):
+        return self.role == 'student'
+    @property
+    def is_academic_supervisor(self):
+        return self.role == 'academic_supervisor'
+    
+    @property
+    def is_workplace_supervisor(self):
+        return self.role == 'workplace_supervisor'
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+    
     def __str__(self):
         return f"{self.username} ({self.role})"
     
@@ -124,4 +137,4 @@ class Evaluation(models.Model):
 
 
     def __str__(self):
-        return f"{self.Evaluator.username} - {self.criteria.name}"
+        return f"{self.evaluator.username} - {self.criteria.name}"
