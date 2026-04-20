7dd7221c6a06fee21fb282528d1531823ebeeafe
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-04-20 16:50:07 +0300

    added a post_save signal for Evaluation

--- backend/internship/signals.py
+++ backend/internship/signals.py
@@ -16,3 +16,18 @@ def create_assessment_notification(sender, instance, created, **kwargs):
             assessment=instance,
             WeeklyLog=instance.log
         )
+
+@receiver(post_save, sender=Evaluation)
+def create_evaluation_notification(sender, instance, created, **kwargs):
+    """Create notification when evaluation is created"""
+    if created:
+        student_user = instance.log.student.user
+        Notification.objects.create(
+            recipient=student_user,
+            notification_type='evaluation',
+            title='Evaluation Feedback Received',
+            message=f'Your Week {instance.log.week_number} work was evaluated on {instance.criteria.name}',
+            evaluation=instance,
+            weekly_log=instance.log
+        )
+ 
