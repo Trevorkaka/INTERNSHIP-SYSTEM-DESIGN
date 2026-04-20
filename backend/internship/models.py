da0f6b437299fd5e2dd2ce2ae34bf0a1ba233a2f
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-04-20 16:04:57 +0300

    added the function of mark_as_read which perfoems a state change it persists the changes to the database

--- backend/internship/models.py
+++ backend/internship/models.py
@@ -169,3 +169,7 @@ class Notification(models.Model);
 
     def __str__(self):
         return f"{self.title} - {self.recipient.username}"
+    
+    def mark_as_read(self):
+        self.is_read = True
+        self.save()
