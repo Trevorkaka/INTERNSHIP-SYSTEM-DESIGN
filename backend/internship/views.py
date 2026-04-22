4b740ff63cc3de759a7739680993558a64d64410
Trevorkaka <kaittotrevor@gmail.com>
2026-04-22 17:05:06 +0300

    authication views

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -132,4 +132,6 @@ class AssessmentViewSet(viewsets.ModelViewSet):
             return Assessment.objects.filter(log__student__user=user)
         return Assessment.objects.all()
     
-    
+ # Authentication Views
+def register_view(request):
+    """Handle user registration"""   
