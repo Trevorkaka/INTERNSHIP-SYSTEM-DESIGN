e16f946149ffeea756bb64bb0177c8a85c1c6163
Trevorkaka <kaittotrevor@gmail.com>
2026-04-22 16:28:45 +0300

    view change on import

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from backend.internship.permissions import IsOwnerOrSupervisorOrAdmin
 from internship.models import (
     User, Student, WorkPlaceSupervisor, AcademicSupervisor,
     WeeklyLog, EvaluationCriteria, Evaluation,
