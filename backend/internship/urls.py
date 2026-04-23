6aa6dcae583fb8aebe7391dfa8ac7ece5d54fa5a
eriapaulmwesigwa <eriapaul40@gmail.com>
2026-04-23 21:44:51 +0300

    added to Urls.py

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -1,20 +1,7 @@
-#from django.urls import path #import views from the current directory
-#from .views import students # import the students view from the views.py file in the current directory
-
-#urlpatterns = [
-    #path('students/', students),
-
-#]
-
-from django.urls import path
-#from django.http import HttpResponse
-from . import views
-
-#def test(request):
-    #return HttpResponse("Internship works")
-
-urlpatterns = [
-    path('register/', views.register_view, name='register'),#to handle user registration
-    path('login/',views.login_view, name='login'), #to handle user login
-    path('logout/', views.logout_view, name='logout'), #to handle user logout
-]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet, StudentViewSet, WorkPlaceSupervisorViewSet,
+    AcademicSupervisorViewSet, WeeklyLogViewSet, EvaluationCriteriaViewSet,
+    EvaluationViewSet, AssessmentViewSet, NotificationViewSet
+)
