f8253d16d204026c7c07f0c3295cd9d89c91ee9c
eriapaulmwesigwa <eriapaul40@gmail.com>
2026-04-23 21:48:34 +0300

    added to Urls.py

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -11,3 +11,5 @@ router = DefaultRouter()
 
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'students', StudentViewSet, basename='student')
+router.register(r'workplace-supervisors', WorkPlaceSupervisorViewSet, basename='workplace-supervisor')
+router.register(r'academic-supervisors', AcademicSupervisorViewSet, basename='academic-supervisor')
