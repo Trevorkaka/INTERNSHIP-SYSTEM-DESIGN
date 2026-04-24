ad6f5d02a72bae168efe07ed357058e98f40659a
suubigeorge <georgesuubi98@gmail.com>
2026-04-24 12:00:41 +0300

    george/settings.py update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iles_db',
+        'USER': 'iles_user',
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
