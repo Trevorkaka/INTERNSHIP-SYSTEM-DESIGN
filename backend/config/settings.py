88a6c52dc75cc31b98fe989dcbe3c41b5ea20654
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-04-25 16:30:48 +0300

    added a few things in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'internship',
+    'rest_framework.authtoken',
     'rest_framework',
+    'internship',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
