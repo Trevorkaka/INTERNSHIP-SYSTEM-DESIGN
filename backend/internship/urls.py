13d6c179c20f498c913ba07e86689e7b2d0e4e91
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 16:10:09 +0300

    jwt api for React

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshview  
-
 from . import views
 
 #-- DRF Router ----------------------------------------------------------------------
@@ -30,6 +29,10 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),#to handle user registration
     path('login/',    views.login_view,    name='login'), #to handle user login
     path('logout/',   views.logout_view,   name='logout'), #to handle user logout
+
+    #--JWT API Auth(for React frontend)
+    #POST{username, password} returns access + refresh tokens +user info
+    path('api/auth/ogin/', views.jwt_login,  name='jwt-login'),
 
     #All REST API endpoints live under /api/
     path('api/', include(router.urls)),
