61c1738a3544ea1debb105c2c744880835f398f1
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 12:40:33 +0300

    more cleaned

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -327,10 +327,6 @@ class CustomAuthToken(ObtainAuthToken):
  
 
 
-class EvaluationCriteriaViewSet(viewsets.ModelViewSet):
-    queryset = EvaluationCriteria.objects.all()
-    serializer_class = EvaluationCriteriaSerializer
-    permission_classes = [IsAdminOrReadOnly] # Only admins can create or edit criteria, but anyone can read them.
 
 class EvaluationViewSet(viewsets.ModelViewSet):
     queryset = Evaluation.objects.all()
