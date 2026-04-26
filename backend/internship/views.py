f0e6046671f9f8d7596d304aea770a09f2dd45f4
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 12:52:42 +0300

    more fix on removal

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -276,25 +276,7 @@ def logout_view(request):
     return redirect('login')
 
 
-        #get unread notifications
-        notifications = Notification.objects.filter(
-            recipient=user,
-            is_read = False
-        )[:5] # last 5 unread
-
-        return Response({
-            'token': token.key,
-            'user': {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'role': user_role
-            },
-            'unread_notifications': NotificationSerializer(notifications, many=True).data,
-            'unread_count': Notification.objects.filter(recipient=user, is_read=False).count()
-        })
+      
     
 
     def _get_user_role(self, user):
