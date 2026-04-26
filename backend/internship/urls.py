e633d002d7faf12f82c5c3a2bfcccab9a1a25cd2
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 13:23:43 +0300

    Well sorted drf

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -12,15 +12,15 @@ from . import views
 #   DELETE /api/students/{id}/     → destroy
 
 router = DefaultRouter()
-router.register(r'users',         views.UserViewSet,                basename='user')
-router.register(r'students',      views.StudentViewSet,             basename='student')
-router.register(r'workplace-supervisors',views.WorkPlaceSupervisorViewSet, basename='workplace-supervisor')
-router.register(r'academic-supervisors', views.AcademicSupervisorViewSet,  basename='academic-supervisor')
-router.register(r'weekly-logs',   views.WeeklyLogViewSet,           basename='weekly-log')
-router.register(r'evaluations',   views.EvaluationViewSet,          basename='evaluation')
-router.register(r'evaluaton-criteria', views.EvaluationCriteriaViewSet, basename='evaluation-criteria')
-router.register(r'assessments',   views.AssessmentViewSet,          basename='assessment')
-router.register(r'notifications', views.NotificationViewSet,        basename='notification')
+router.register(r'users',                 views.UserViewSet,                basename='user')
+router.register(r'students',              views.StudentViewSet,             basename='student')
+router.register(r'workplace-supervisors', views.WorkPlaceSupervisorViewSet, basename='workplace-supervisor')
+router.register(r'academic-supervisors',  views.AcademicSupervisorViewSet,  basename='academic-supervisor')
+router.register(r'weekly-logs',           views.WeeklyLogViewSet,           basename='weekly-log')
+router.register(r'evaluations',           views.EvaluationViewSet,          basename='evaluation')
+router.register(r'evaluaton-criteria',    views.EvaluationCriteriaViewSet,  basename='evaluation-criteria')
+router.register(r'assessments',           views.AssessmentViewSet,          basename='assessment')
+router.register(r'notifications',         views.NotificationViewSet,        basename='notification')
 
 
 urlpatterns = [
