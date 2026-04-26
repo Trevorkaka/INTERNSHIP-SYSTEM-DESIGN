3db0ff06e1a76c45f1833223a64341b00852f0b0
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 12:57:21 +0300

    REMOVED tOKEN IMPORT

--- backend/internship/views.py
+++ backend/internship/views.py
@@ -2,7 +2,6 @@ from django.utils import timezone
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
