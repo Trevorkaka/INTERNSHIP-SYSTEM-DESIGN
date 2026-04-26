fa89bb81e7504606f22a7ceec30ce1dd0d096852
Trevorkaka <kaittotrevor@gmail.com>
2026-04-26 13:07:40 +0300

    internship/urls.py

--- backend/internship/urls.py
+++ backend/internship/urls.py
@@ -1,13 +1,5 @@
-#from django.urls import path #import views from the current directory
-#from .views import students # import the students view from the views.py file in the current directory
-
-#urlpatterns = [
-    #path('students/', students),
-
-#]
-
-from django.urls import path
-#from django.http import HttpResponse
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 #def test(request):
