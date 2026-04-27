dab9bda33b2b71b797203531a8dcc4d843724395
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-04-27 17:08:37 +0300

    added api.interceptors.request.use

--- frontend/src/components/dashboards/API.js
+++ frontend/src/components/dashboards/API.js
@@ -2,5 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: '/api',
-    headers: {'Content-Type':'application/json'}
+    headers: {'Content-Type':'application/json'},
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access')
 })
