12773d5700ea0601468568e9939f310e60af40e4
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-04-27 17:10:38 +0300

    finalized with the API.js file

--- frontend/src/components/dashboards/API.js
+++ frontend/src/components/dashboards/API.js
@@ -7,4 +7,8 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('access')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
 })
+
+export default api
