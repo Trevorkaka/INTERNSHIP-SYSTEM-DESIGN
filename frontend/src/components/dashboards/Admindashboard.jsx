938ac292daeb38f845ca93e0c3bd1bf698245a31
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-04-28 14:54:19 +0300

    imported list and dashboard.css

--- frontend/src/components/dashboards/Admindashboard.jsx
+++ frontend/src/components/dashboards/Admindashboard.jsx
@@ -1,3 +1,6 @@
 import { useState } from "react";
-import api from '../../utils/api'
-import card from '../Shared/Card'
+import api from '../../utils/api';
+import card from '../Shared/Card';
+import List from '../Shared/list';
+import '../styles/Dashboard.css';
+
