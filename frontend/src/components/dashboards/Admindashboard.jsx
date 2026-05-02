511eaaa90b3174f1b064231f450072894889a461
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-05-02 15:51:43 +0300

    fixed card to Card

--- frontend/src/components/dashboards/Admindashboard.jsx
+++ frontend/src/components/dashboards/Admindashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from '../../utils/api';
-import card from '../Shared/Card';
+import Card from '../Shared/Card';
 import List from '../Shared/list';
 import '../styles/Dashboard.css';
 
