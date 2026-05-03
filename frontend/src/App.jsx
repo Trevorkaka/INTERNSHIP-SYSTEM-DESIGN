d98f50c173c1b5390738b6bb96bdd46f196aae0b
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-05-03 09:26:34 +0300

    fixing login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { StudentDashboard, WorkplaceSupervisorDashboard } from './components/dashboards'
-import  Login  from './components/auth/dashboards'
+import { StudentDashboard, WorkplaceSupervisorDashboard} from './components/auth/dashboards'
 
 function App() {
   const [view, setView] = useState('home')
