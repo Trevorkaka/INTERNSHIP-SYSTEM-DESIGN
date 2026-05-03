e2ce25bd7f489fcac3e4b8385cfa189d466f1e92
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-05-03 09:32:22 +0300

    trying to fix the lists in studentdashboard

--- frontend/src/components/dashboards/dashboards.jsx
+++ frontend/src/components/dashboards/dashboards.jsx
@@ -1,4 +1,4 @@
-export { default as StudentDashboard } from '../StudentDashboard';
-export { default as WorkplaceSupervisorDashboard } from '../WorkplaceSupervisorDashboard';
-//export { default as AcademicSupervisorDashboard } from './AcademicSupervisorDashboard';
-export { default as Login } from '/Login';
+export { default as StudentDashboard } from '../dashboards/StudentDashboard';
+export { default as WorkplaceSupervisorDashboard } from '../dashboards/WorkplaceSupervisorDashboard';
+//export { default as AcademicSupervisorDashboard } from '../dashboards/AcademicSupervisorDashboard';
+export { default as Login } from '../auth/Login';
