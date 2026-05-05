20eb6a0bc2bd613636ec4ed065ecbce3be4daa76
Trevorkaka <kaittotrevor@gmail.com>
2026-05-05 08:36:22 +0300

    interface authcontext type

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -10,3 +10,12 @@ interface User {
   last_name: string
   role: 'student' | 'academic_supervisor' | 'workplace_supervisor' | 'admin'
 }
+
+
+interface AuthContextType {
+  user: User | null
+  isAuthenticated: boolean
+  isLoading: boolean
+  login: (username: string, password: string) => Promise<void>
+  logout: () => Promise<void>
+}
