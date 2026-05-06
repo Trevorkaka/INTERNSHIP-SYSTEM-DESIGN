7f085f92b484deb0f8d0a5bcc9cf5b8dcb38c2c3
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-05-06 11:58:06 +0300

    added REST_FRAMEWORK config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'internship',
     'rest_framework',
+    'REST_FRAMEWORK config',
     'django-cors-header',
     'rest_framework_simplejwt',        
     'rest_framework_simplejwt.token_blacklist',
