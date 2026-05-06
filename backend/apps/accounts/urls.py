8e42c3d081a0b1f7bf5bb7d15f3e0e6646f96bfd
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-05-06 11:55:23 +0300

    loaded the url patterns

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = 'accounts'
 
-urlpatterns = []
+urlpatterns = [
+    path('auth/signup/', views.signup, name='signup'),
+    path('auth/login/', views.login, name='login'),
+    path('auth/logout/', views.logout, name='logout'),
+    path('auth/refresh/', views.refresh_token, name='refresh'),
+]
