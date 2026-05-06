4e3b7b2d4746f67ab17e2030df4e4b80e3e55805
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-05-06 11:15:58 +0300

    added class user sign up serializer

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -4,3 +4,10 @@ from django.core.validators import EmailValidator
 from django.db import transaction
 
 User = get_user_model()
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True, min_length=8)
+    email = serializers.EmailField(validators=[EmailValidator()])
+    
