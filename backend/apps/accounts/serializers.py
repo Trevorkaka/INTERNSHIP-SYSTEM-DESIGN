f3c28cc04f0b4d7b8734fe96933afe23d98d2a2e
MUGALU BENJAMIN <mugalubenjamkin745@gmail.com>
2026-05-06 11:14:37 +0300

    added user

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -2,3 +2,5 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.core.validators import EmailValidator
 from django.db import transaction
+
+User = get_user_model()
