2357f2bd4e9dcce812844bbecd6bc4813889755a
cesto1-matt <ongolcesto2018@gmail.com>
2026-06-04 00:38:53 +0300

    worked on

--- backend/internship/permissions.py
+++ backend/internship/permissions.py
@@ -21,6 +21,8 @@ class IsAdminOrSelf(BasePermission):
 
 
 
+
+
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_student
