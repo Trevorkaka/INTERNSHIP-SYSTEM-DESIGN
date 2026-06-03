6aa2630eb7b696f208265fc2a0f8e55e380d0585
cesto1-matt <ongolcesto2018@gmail.com>
2026-06-04 00:44:46 +0300

    okkkk

--- backend/internship/apps.py
+++ backend/internship/apps.py
@@ -5,7 +5,10 @@ class InternshipConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'internship'
 
-    
+
 
     def ready (self):
         import internship.signals
+
+
+
