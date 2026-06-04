70273cc454fd069de23aad49eb018fa25ca46fb9
MUGALU BENJAMIN <mugalubenjamin745@gmail.com>
2026-06-04 20:11:33 +0300

    yes merge

--- frontend/src/components/dashboards/AcademicSupervisorDashboard.tsx
+++ frontend/src/components/dashboards/AcademicSupervisorDashboard.tsx
@@ -2,3 +2,25 @@ import { useState, useEffect } from 'react'
 import { Loader, ClipboardCheck, FileText, BarChart2, Download } from 'lucide-react'
 import { evaluationsAPI } from '../../api/services'
 import client from '../../api/client'
+
+interface Student {
+  id: number
+  registration_number: string
+  course: string
+  year_of_study: number
+  user: { id: number; username: string; first_name: string; last_name: string }
+}
+ 
+interface Log {
+  id: number
+  week_number: number
+  status: string
+  student: number
+  submitted_at: string | null
+}
+ 
+interface Criteria {
+  id: number
+  name: string
+  max_score: number
+}
