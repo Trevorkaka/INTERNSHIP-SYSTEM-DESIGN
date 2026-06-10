27bb64741a3a9f1dfd8cfe562dc33d11b2d76990
cesto1-matt <ongolcesto2018@gmail.com>
2026-06-10 22:43:06 +0300

    SeriaUpdate

--- backend/apps/logs/serializers.py
+++ backend/apps/logs/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import WeeklyLog, Assessment
 
-
 class WeeklyLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeeklyLog
