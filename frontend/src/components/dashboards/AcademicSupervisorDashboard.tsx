b72de7b067f71ee38b46a99d0cafe62b3c169f42
Trevorkaka <kaittotrevor@gmail.com>
2026-06-12 13:06:58 +0300

    still on navigation

--- frontend/src/components/dashboards/AcademicSupervisorDashboard.tsx
+++ frontend/src/components/dashboards/AcademicSupervisorDashboard.tsx
@@ -345,6 +345,7 @@ export default function AcademicSupervisorDashboard({ filter = 'all' }: { filter
       <div className="p-4 space-y-3">
         {students.length === 0 && (
           <div key={s.id} className="p-5 border-2 border-gray-100 hover:border-blue-200 rounded-xl transition-all">
+            <div className="flex items-start justify-between mb-4">
       </div>
 
    {/* Evaluations view */}
